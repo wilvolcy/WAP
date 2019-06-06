9eb3fea274ede364632189fcc907a755f09b2bd8
wilvolcy <50376806+wilvolcy@users.noreply.github.com>
2019-06-06 14:32:55 -0500

    test

---
+++ homeworks/output.js
@@ -0,0 +1,28 @@
+"use strict";
+
+$(function() {
+	$('#hw').change(function() {
+        // var file = 'homeworks/'+$('#hw').val();
+         var file = 'https://github.com/wilvolcy/WAP/tree/master/'+$('#hw').val();
+        
+		$.ajax({
+            'url': file,
+            // 'url' :'https://github.com/wilvolcy/WAP/blob/master/Images/index.html',
+			'type': 'GET',
+			'success': ajaxSuccess,
+			'error': ajaxFailure
+		});
+	});
+});
+
+function ajaxSuccess(data) {
+	$('#output').val(data);
+}
+
+function ajaxFailure(xhr, status, exception) {
+  console.log(xhr, status, exception);
+}
+
+// alert("test");
+
+// $.get('https://www.google.com/images/branding/product/ico/googleg_lodp.ico');
